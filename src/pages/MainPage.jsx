f4540d693150410cee5e9d16f9907aec47d37698
DongHyeon <skehdgus401@gmail.com>
2022-07-10 02:59:14 +0900

    feat: 메인페이지 가로스크롤 구현

--- src/pages/MainPage.jsx
+++ src/pages/MainPage.jsx
@@ -6,13 +6,36 @@ import Header from "./Header.js";
 import MainImg1 from "../img/MainImg1.png";
 import MainImg2 from "../img/MainImg2.png";
 import AddClothButton from "../img/AddClothButton.png";
+import { ScrollMenu } from "react-horizontal-scrolling-menu";
+import Outfit1Img from "../img/Outfit1.png";
+import Outfit2Img from "../img/Outfit2.png";
+import Outfit3Img from "../img/Outfit3.png";
+import Outfit4Img from "../img/Outfit4.png";
 import axios from "axios";
 
 const MainPage = () => {
   const navigate = useNavigate();
+  // Item 단위의 컴포넌트
+  const MenuItem = ({ text }) => {
+    console.log("dddd");
+    return (
+      <div className="menu-item" style={{ color: "white" }}>
+        {text}
+      </div>
+    );
+  };
+
+  // MenuItem들을 list만큼 생성한 List단위의 컴포넌트
+  const Menu = (list) =>
+    list.map((el) => {
+      const { name } = el;
+      return <MenuItem text={name} key={name} />;
+    });
   const onClickLogo = () => {
     navigate("/");
   };
+
+  const [DataItem, setDataItem] = useState(null);
 
   return (
     <>
@@ -30,15 +53,26 @@ const MainPage = () => {
         </BodyContainer>
         <FootContainer>
           <Line />
+          <FooterMainFont>이런 코디 어때요?</FooterMainFont>
+          <ScrollContainer>
+            <OutfitImg src={Outfit1Img}></OutfitImg>
+            <OutfitImg src={Outfit2Img}></OutfitImg>
+            <OutfitImg src={Outfit3Img}></OutfitImg>
+            <OutfitImg src={Outfit4Img}></OutfitImg>
+          </ScrollContainer>
         </FootContainer>
       </Container>
     </>
   );
 };
-
+const OutfitImg = styled.img`
+  width: 24.06em;
+  height: 34.63em;
+  padding: 20px;
+`;
 const Container = styled.div`
   width: 100%;
-  height: 200em;
+  height: 123.2em;
   background-color: #2c2c2c;
 `;
 const BodyContainer = styled.div`
@@ -49,7 +83,20 @@ const BodyContainer = styled.div`
 `;
 const FootContainer = styled.div`
   display: flex;
+  flex-direction: column;
   justify-content: center;
+  align-items: center;
+`;
+const ScrollContainer = styled.div`
+  margin-top: 43px;
+  width: 100%;
+  padding: 20px;
+  overflow: scroll;
+  color: #112031;
+
+  /* 가로 스크롤 */
+  overflow: auto;
+  white-space: nowrap;
 `;
 const Line = styled.div`
   height: 3px;
@@ -117,6 +164,13 @@ const RegisterButton = styled.img`
   margin-top: 967px;
   right: 55px;
   cursor: pointer;
+`;
+
+const FooterMainFont = styled.div`
+  font-family: GmarketSans-Bold;
+  font-size: 3.13em;
+  color: white;
+  margin-top: 59px;
 `;
 
 export default MainPage;
