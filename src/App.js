70cc4ee6bc30f853d158df0f1f6e022384ebcad3
DongHyeon <skehdgus401@gmail.com>
2022-07-09 13:20:38 +0900

    feat: 폰트 셋팅

--- src/App.js
+++ src/App.js
@@ -1,8 +1,14 @@
 import logo from "./logo.svg";
 import "./App.css";
-
+import React, { useEffect, useContext, useState } from "react";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import Calendar from "./pages/Calendar.js";
 function App() {
-  return <div className="App"></div>;
+  return (
+    <Routes>
+      <Route path="/" element={Calendar}></Route>
+    </Routes>
+  );
 }
 
 export default App;
